89e7870dcef3df40131eb6f71935a93302f0966a
JosefineSvarre <josefinesvarre@hotmail.com>
2025-10-14 14:13:48 +0200

    Add element selectors for login modal functionality in login.js

--- js/login.js
+++ js/login.js
@@ -4,4 +4,7 @@
 // ------------------------------------------ //
 
 // Henter elementer fra DOM -----------------
-const
+const loginBtn = document.querySelecto(".header__icon-btn"); // Hent login-knap
+const loginModal = document.getElementById("loginModal"); // Hent modal
+const closeBtn = document.querySelector(".login-modal__close"); // Hent luk-knap
+const loginForm = document.getElementById("loginForm"); // Hent login-formular
